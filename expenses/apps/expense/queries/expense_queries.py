cfc5504f6aede66fea9a00cc741372fb3f2bf59d
alainburindi <alainburindi62@gmail.com>
2020-02-17 12:31:48 +0200

    allow user to filter their expenses

--- expenses/apps/expense/queries/expense_queries.py
+++ expenses/apps/expense/queries/expense_queries.py
@@ -7,8 +7,11 @@ from ..types import ExpenseType
 
 class Query(graphene.ObjectType):
 
-    my_expenses = graphene.List(ExpenseType)
+    my_expenses = graphene.List(ExpenseType, search=graphene.String())
 
     @login_required
-    def resolve_my_expenses(self, info):
-        return info.context.user.expense_set.all()
+    def resolve_my_expenses(self, info, **kwargs):
+        search = kwargs.get('search')
+        user_expenses = info.context.user.expense_set
+        return user_expenses.filter(name__icontains=search) \
+            if search else user_expenses.all()
--- expenses/apps/plan/queries/plan_queries.py
+++ expenses/apps/plan/queries/plan_queries.py
@@ -13,4 +13,5 @@ class Query(graphene.ObjectType):
     def resolve_my_plans(self, info, **kwargs):
         search = kwargs.get('search')
         user_plans = info.context.user.plan_set
-        return user_plans.filter(name__icontains=search) if search else user_plans.all()
+        return user_plans.filter(name__icontains=search) \
+            if search else user_plans.all()
